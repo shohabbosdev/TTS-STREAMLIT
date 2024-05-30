a75bcab468ee7ad774602426c429a6d14b43f2f8
shohabbosdev <71746304+freedom-1@users.noreply.github.com>
2024-05-30 09:11:11 +0500

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import requests
 from latin_cyrillic_symbols import to_cyrillic
 from tqdm import tqdm
 
+api_key = st.secret["API_TOKEN"]
 # API URL and headers
 API_URL = "https://api-inference.huggingface.co/models/shohabbosdev/text-to-audio"
 headers = {"Authorization": f"Bearer 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"}
